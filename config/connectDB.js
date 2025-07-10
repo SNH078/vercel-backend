65f3342c4e0d6b39f2c1c5a7f4e01a78ead6e1b5
SNH115577 <164773937+SNH078@users.noreply.github.com>
2025-07-10 22:14:30 +0530

    b commit

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,5 +1,4 @@
 import mongoose, { mongo } from "mongoose";
-
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -9,3 +8,29 @@ export const connectDB = async () => {
     process.exit(1); 
   }
 };
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config(); // Load environment variables
+
+// const ATLAS_URI = process.env.MONGO_URI;
+// const LOCAL_URI = process.env.MONGO_URI_c;
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(ATLAS_URI);
+//     console.log("✅ Connected to MongoDB Atlas");
+//   } catch (error) {
+//     console.warn("⚠️ Atlas connection failed. Trying local MongoDB...");
+
+//     try {
+//       await mongoose.connect(LOCAL_URI);
+//       console.log("✅ Connected to local MongoDB (Compass)");
+//     } catch (err) {
+//       console.error("❌ Failed to connect to both Atlas and local MongoDB.");
+//       console.error(err);
+//       process.exit(1);
+//     }
+//   }
+// };
